f84614e70d1fd652d5976d941dad16b692213f99
L4tabar <l4tabar@gmail.com>
2019-05-15 17:41:49 +0200

    Tesztek javítása

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -10,11 +10,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Shapes;
-
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import model.TimerClass;
 import model.Users;
 import dao.UsersDao;
@@ -36,9 +34,7 @@ public class Game {
 
     public void start(Stage primaryStage) throws InterruptedException {
 
-        AtomicBoolean same = new AtomicBoolean(false);
-        Random rand = new Random();
-
+        AtomicBoolean same = new AtomicBoolean(false);;
 
         TimerClass tc = new TimerClass();
         tc.startTimer();
@@ -66,106 +62,113 @@ public class Game {
         same.set(getScene(iv1, iv2, shapes, random(), random()));
         scene.setOnKeyPressed(event -> {
 
-            if (i.get() == 10) {
+            if(i.get()>10) {
+                System.out.println("Vége");
+            }
+            else{
+                if (i.get() == 10) {
 
-                double reactT = (time[0] + time[1] + time[2] + time[3] + time[4] + time[5] + time[6] + time[7] + time[8] + time[9]) / 10;
+                    double reactT = (time[0] + time[1] + time[2] + time[3] + time[4] + time[5] + time[6] + time[7] + time[8] + time[9]) / 10;
 
-                Text reactTime = new Text();
-                reactTime.setTranslateX(-400);
-                reactTime.setTranslateY(-250);
-                reactTime.setStyle("-fx-font: 24 arial;");
-                root.getChildren().add(reactTime);
-                reactTime.setText(String.valueOf(reactT));
+                    Text reactTime = new Text();
+                    reactTime.setTranslateX(-400);
+                    reactTime.setTranslateY(-250);
+                    reactTime.setStyle("-fx-font: 24 arial;");
+                    root.getChildren().add(reactTime);
+                    reactTime.setText(String.valueOf(reactT));
 
-                addPlayer(username.getText(), reactT);
-                System.out.println("A te reakcióidőd: " + reactT);
+                    addPlayer(username.getText(), reactT);
+                    System.out.println("A te reakcióidőd: " + reactT);
+                    i.incrementAndGet();
+
+                }
+                if(event.getCode() == KeyCode.F) {
+
+                    if(!same.get()) {
+                        if (i.get() == 0) {
+
+                            time[i.get()] = tc.getSecondsPassed();
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Az első nem volt egyforma: ");
+
+                        }
+
+                        else {
+
+                            time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1];
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Nem volt egyforma: ");
+
+                        }
+                    }
+
+                    if(same.get()) {
+
+                        if (i.get() == 0) {
+                            time[i.get()] = tc.getSecondsPassed() + 2;
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Az első helytelen: ");
+
+                        }
+                        else {
+
+                            time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1] + 2;
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Helytelen: ");
+
+                        }
+                    }
+                }
+                if(event.getCode() == KeyCode.J) {
+
+                    if(same.get()) {
+
+                        if (i.get() == 0) {
+
+                            time[i.get()] = tc.getSecondsPassed();
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Az első egyforma: ");
+
+                        }
+                        else {
+
+                            time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1];
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Egyforma: ");
+
+                        }
+
+                    }
+
+                    if(!same.get()) {
+
+                        if (i.get() == 0) {
+
+                            time[i.get()] = tc.getSecondsPassed() + 2;
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Az első helytelen válasz: ");
+
+                        }
+                        else {
+
+                            time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1] + 2;
+                            time_tmp[i.get()] = tc.getSecondsPassed();
+                            System.out.println("Helytelen válasz: ");
+
+                        }
+                    }
+                }
+
+                if(event.getCode() == KeyCode.Q) {
+                    root.getChildren().clear();
+                    primaryStage.close();
+                }
+
+                same.set(getScene(iv1, iv2, shapes, random(), random()));
                 i.incrementAndGet();
-
-            }
-            if(event.getCode() == KeyCode.F) {
-
-                if(!same.get()) {
-                    if (i.get() == 0) {
-
-                        time[i.get()] = tc.getSecondsPassed();
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Az első nem volt egyforma: ");
-
-                    }
-
-                    else {
-
-                        time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1];
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Nem volt egyforma: ");
-
-                    }
-                }
-
-                if(same.get()) {
-
-                    if (i.get() == 0) {
-                        time[i.get()] = tc.getSecondsPassed() + 2;
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Az első helytelen: ");
-
-                    }
-                    else {
-
-                        time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1] + 2;
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Helytelen: ");
-
-                    }
-                }
-            }
-            if(event.getCode() == KeyCode.J) {
-
-                if(same.get()) {
-
-                    if (i.get() == 0) {
-
-                        time[i.get()] = tc.getSecondsPassed();
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Az első egyforma: ");
-
-                    }
-                    else {
-
-                        time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1];
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Egyforma: ");
-
-                    }
-
-                }
-
-                if(!same.get()) {
-
-                    if (i.get() == 0) {
-
-                        time[i.get()] = tc.getSecondsPassed() + 2;
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Az első helytelen válasz: ");
-
-                    }
-                    else {
-
-                        time[i.get()] = tc.getSecondsPassed() - time_tmp[i.get()-1] + 2;
-                        time_tmp[i.get()] = tc.getSecondsPassed();
-                        System.out.println("Helytelen válasz: ");
-
-                    }
-                }
             }
 
-            if(event.getCode() == KeyCode.Q) {
-                root.getChildren().clear();
-                primaryStage.close();
-            }
 
-            same.set(getScene(iv1, iv2, shapes, random(), random()));
-            i.incrementAndGet();
 
         });
 
--- src/test/java/SameShapeTest.java
+++ src/test/java/SameShapeTest.java
@@ -2,11 +2,12 @@ import controller.Game;
 import javafx.scene.image.ImageView;
 import model.Shapes;
 import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class sameShapeTesting {
+public class SameShapeTest extends ApplicationTest {
     @Test
     void testShapes (){
         Game tester = new Game();
@@ -15,14 +16,14 @@ public class sameShapeTesting {
         ImageView iv1 = new ImageView();
         ImageView iv2 = new ImageView();
 
+        assertTrue(tester.getScene(iv1,iv2,shapes,0,0));
         assertTrue(tester.getScene(iv1,iv2,shapes,1,1));
         assertTrue(tester.getScene(iv1,iv2,shapes,2,2));
-        assertTrue(tester.getScene(iv1,iv2,shapes,3,3));
+        assertFalse(tester.getScene(iv1,iv2,shapes,0,1));
+        assertFalse(tester.getScene(iv1,iv2,shapes,0,2));
+        assertFalse(tester.getScene(iv1,iv2,shapes,1,0));
         assertFalse(tester.getScene(iv1,iv2,shapes,1,2));
-        assertFalse(tester.getScene(iv1,iv2,shapes,1,3));
-        assertFalse(tester.getScene(iv1,iv2,shapes,2,3));
+        assertFalse(tester.getScene(iv1,iv2,shapes,2,0));
         assertFalse(tester.getScene(iv1,iv2,shapes,2,1));
-        assertFalse(tester.getScene(iv1,iv2,shapes,3,1));
-        assertFalse(tester.getScene(iv1,iv2,shapes,3,2));
     }
 }
